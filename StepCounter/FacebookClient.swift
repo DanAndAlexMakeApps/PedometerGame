1287c4b5d90a22fa8d8f428b603a190a7a3a5f20
damargulis <damargulis@wustl.edu>
2016-03-03 19:04:22 -0600

    Added clients

---
+++ StepCounter/FacebookClient.swift
@@ -0,0 +1,13 @@
+//
+//  FacebookClient.swift
+//  StepCounter
+//
+//  Created by Daniel Margulis on 3/3/16.
+//  Copyright © 2016 Alex Oser. All rights reserved.
+//
+
+import UIKit
+
+class FacebookClient: BDBOAuth1Credential {
+
+}
---
+++ StepCounter/TwitterClient.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.swift
+//  StepCounter
+//
+//  Created by Daniel Margulis on 3/3/16.
+//  Copyright © 2016 Alex Oser. All rights reserved.
+//
+
+import UIKit
+
+class TwitterClient: BDBOAuth1Credential {
+
+}
